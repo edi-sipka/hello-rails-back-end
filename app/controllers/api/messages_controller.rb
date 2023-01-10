672fc2815f1a827f08f6944ae065dccb30e286fd
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-10 14:30:49 +0100

    Fix linters

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::MessagesController < ApplicationController
-      def index
-        messages = Message.all.shuffle
-        render json: messages
-      end
-    end
+  def index
+    messages = Message.all.shuffle
+    render json: messages
+  end
+end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: %i[ show update destroy ]
+  before_action :set_message, only: %i[show update destroy]
 
   # GET /messages
   def index
@@ -39,13 +39,14 @@ class MessagesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_message
-      @message = Message.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def message_params
-      params.require(:message).permit(:greeting)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def message_params
+    params.require(:message).permit(:greeting)
+  end
 end
